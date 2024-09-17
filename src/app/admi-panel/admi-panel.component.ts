1e57ea44924193c88001a6a624773542c36a2077
EdgardoJorge <i2310402@continental.edu.pe>
2024-09-16 21:25:19 -0500

    version1

---
+++ src/app/admi-panel/admi-panel.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-admi-panel',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './admi-panel.component.html',
+  styleUrl: './admi-panel.component.css'
+})
+export class AdmiPanelComponent {
+
+}
